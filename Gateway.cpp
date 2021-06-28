a806938b9bbd856144eef80cd49ded3c104cd02d
maroprjs <maroprjs@gmail.com>
2021-06-28 09:48:45 +0200

    add shutdown functionality to gw with serial test

--- Gateway.cpp
+++ Gateway.cpp
@@ -11,8 +11,9 @@ Gateway::Gateway(LatchingRelay* pwr) {
 	_pwr = pwr;
     _gatewayIsOff = true;
     _countDownStepMillis = 0;
-	_countDown = STARTUP_COUNTDOWN;
-	_countDownEnabled = false;
+	_countDown = LONG_COUNTDOWN;
+	_shutdownOngoing = false;
+	_lastCmd = "";
 
 }
 
@@ -21,24 +22,43 @@ void Gateway::begin(){
 }
 
 void Gateway::loop(){
-
-	if (_countDownEnabled == true){
+	byte ch;
+	if (_shutdownOngoing == true){
 		if (_countDown > 0 ){
 		   if ((_countDownStepMillis + COUNTDOWN_TIME) < millis()){
               _countDown--;
               _countDownStepMillis = millis();
 		   }
-		}else {
-			turnOff();
-			stopCountDown();
+		}
+		if (_countDown <= SHORT_COUNTDOWN ){ //sends request to real gw to shutdown
+			Serial5.println("shutdown"); //TODO: is it okay to send every sec or should I add flag?
+		}
+		if (_countDown <= 0 ){
+			Serial.println("gw _pwr->off()");
+			//_pwr->off();
+			_gatewayIsOff = true;
+			stopShutDown();
 		}
 	}
 
 	if (Serial5.available()){
-		//if stop countdown
-		Serial.write(Serial5.read());
-		//stopCountDown();
-		//startCountDown(SHUTDOWN_COUNTDOWN);
+		ch = Serial5.read();
+		_lastCmd += (char)ch;
+		if (ch=='\r') {
+			_lastCmd.trim();
+			//if stop countdown
+			Serial.println(_lastCmd);
+			Serial5.println(_lastCmd); //mirror
+			//TODO:
+			if (_lastCmd == "stop_shutdown"){
+			   stopShutDown();
+			};
+			if (_lastCmd == "start_shutdown"){
+			   startShutDown(SHORT_COUNTDOWN);
+			};
+			_lastCmd = "";
+		}
+
 
 	}
 }
@@ -46,14 +66,13 @@ void Gateway::turnOn(){
 	Serial.println("gw _pwr->on()");
 	//_pwr->on();
 	_gatewayIsOff = false;
-    startCountDown(STARTUP_COUNTDOWN);
+    startShutDown(LONG_COUNTDOWN);
 }
 
 void Gateway::turnOff(){
-	Serial.println("gw _pwr->off()");
-	//_pwr->off();
-	_gatewayIsOff = true;
-
+	if (_shutdownOngoing == false){
+		startShutDown(SHORT_COUNTDOWN);
+	}
 }
 
 void Gateway::toggle(){
@@ -64,14 +83,15 @@ void Gateway::toggle(){
 	};
 }
 
-void Gateway::startCountDown(unsigned long countdown){
+void Gateway::startShutDown(unsigned long countdown){
 	_countDownStepMillis = millis();
 	_countDown = countdown;
-    _countDownEnabled = true;
+    _shutdownOngoing = true;
 }
 
-void Gateway::stopCountDown(){
-    _countDownEnabled = false;
+void Gateway::stopShutDown(){
+    _shutdownOngoing = false;
+
 }
 
 Gateway::~Gateway() {
--- Gateway.h
+++ Gateway.h
@@ -8,8 +8,8 @@
 #ifndef GATEWAY_H_
 #define GATEWAY_H_
 #include "LatchingRelay.h"
-#define STARTUP_COUNTDOWN 780 //13min
-#define SHUTDOWN_COUNTDOWN 180 //3min
+#define LONG_COUNTDOWN 780 //13min
+#define SHORT_COUNTDOWN 180 //3min
 #define COUNTDOWN_TIME 1000 //ms
 class Gateway {
 public:
@@ -21,13 +21,14 @@ public:
 	void toggle();
 	virtual ~Gateway();
 private:
-    void startCountDown(unsigned long countdown);
-    void stopCountDown();
+    void startShutDown(unsigned long countdown);
+    void stopShutDown();
 	LatchingRelay* _pwr;
 	bool _gatewayIsOff;
 	unsigned long _countDownStepMillis;
 	unsigned long _countDown;
-	bool _countDownEnabled;
+	bool _shutdownOngoing;
+	String _lastCmd;
 
 
 };
